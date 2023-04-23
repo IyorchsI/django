5f6476e342102e663112f213df4aef296f460b1a
IyorchsI <joanrosero@unicauca.edu.co>
2023-04-23 18:55:27 -0500

    v2

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ set -o errexit
 
 # poetry install
 
-#pip install -r requirements.txt
+pip install -r requirements.txt
+
+gunicorn CrudDj.wsgi
 
 python manage.py collectstatic --no-input
 python manage.py migrate
