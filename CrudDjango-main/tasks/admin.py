36b4a45bd600980e8a5ea8074db0af6cb8aacb79
IyorchsI <104506102+IyorchsI@users.noreply.github.com>
2023-04-23 16:40:24 -0500

    Delete CrudDjango-main directory

--- CrudDjango-main/tasks/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Task#traemos el modelo
-
-# Register your models here.
-#le damos acceso al administrador al modelo tarea 
-admin.site.register(Task)
